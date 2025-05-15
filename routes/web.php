3b941c6b2d4531cf055471b600a7fddc8a6e5af7
Jeefos <152890285+Jeefos@users.noreply.github.com>
2025-05-15 14:17:52 -0400

    web.php, and OrderController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/checkout', [OrderController::class, 'checkoutForm']);
+Route::post('/checkout', [OrderController::class, 'placeOrder']);
