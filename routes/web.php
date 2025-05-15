7b796dcbeda608146ec736d548b8d1c2f06047d7
Malick-Konate <malickzie11@gmail.com>
2025-05-15 14:26:37 -0400

    to check

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/orders', [AdminController::class, 'orders'])->middleware('auth');
+
